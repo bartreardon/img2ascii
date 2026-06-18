30f814a76aed38c504e47ed9c19db1db7cfd3555
Bart Reardon <bart.reardon@csiro.au>
2026-06-18 21:06:08 +1000

    Initial Commit

---
+++ Image 2 ASCII/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image 2 ASCII
+//
+//  Created by Bart E Reardon on 18/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image 2 ASCII/Image_2_ASCIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_2_ASCIIApp.swift
+//  Image 2 ASCII
+//
+//  Created by Bart E Reardon on 18/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct Image_2_ASCIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
